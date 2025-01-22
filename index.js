5bbd245b9a15c7902e192fb653e1b7e9c3fca450
davidhartsough <hartsoughdavid@gmail.com>
2025-01-22 14:17:39 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const url = "https://eoo2jupsehe9km7.m.pipedream.net";
+// const url = "https://eorhfjpils7ry4q.m.pipedream.net";
+
+async function sendMessage(data = {}) {
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  await fetch(url, {
+    method: "POST",
+    headers,
+    mode: "cors",
+    body: JSON.stringify(data),
+  });
+}
+
+const getEl = (id) => document.getElementById(id);
+const getVal = (id) => getEl(id).value;
+const hide = (id) => {
+  getEl(id).style.display = "none";
+};
+const show = (id) => {
+  getEl(id).style.display = "block";
+};
+const form = getEl("form");
+const loadingDiv = getEl("loading");
+const successDiv = getEl("success");
+const spinnerDiv = getEl("spinner");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = {
+    name: getVal("name"),
+    email: getVal("email"),
+    phone: getVal("phone"),
+    message: getVal("message"),
+  };
+  form.style.display = "none";
+  loadingDiv.style.display = "block";
+  spinnerDiv.innerHTML = '<div class="spinner"></div>';
+  sendMessage(data).then(() => {
+    spinnerDiv.innerHTML = "";
+    loadingDiv.style.display = "none";
+    successDiv.style.display = "block";
+  });
+  return false;
+});
+
+const servicesSection = getEl("services");
+const stickyPoint =
+  servicesSection.getBoundingClientRect().top + window.scrollY - 64;
+const nav = getEl("nav");
+const logo = getEl("nav-logo");
+function checkScroll() {
+  const curr = window.scrollY;
+  if (curr > stickyPoint) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+}
+window.addEventListener("scroll", checkScroll);
+checkScroll();
